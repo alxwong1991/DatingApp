85b6db45925b48163e0267878bfe67e55437e498
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-06-12 12:32:52 +0800

    Update client

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,7 +18,7 @@ namespace API
 {
     public class Startup
     {
-        private readonly IConfiguration _config; 
+        private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
@@ -35,10 +35,11 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
 
-            services.AddDbContext<DataContext>(options => 
+            services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
