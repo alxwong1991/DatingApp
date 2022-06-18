630597626ae013fdba1f31d728fabc611500bc1e
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-06-18 13:52:20 +0800

    Done error handling

--- Client/src/app/_guards/auth.guard.ts
+++ Client/src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { map, Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
@@ -8,7 +8,7 @@ import { AccountService } from '../_services/account.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private accountService: AccountService, private toastr: ToastrService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   canActivate(): Observable<boolean> {
     return this.accountService.currentUser$.pipe(
@@ -18,5 +18,5 @@ export class AuthGuard implements CanActivate {
       })
     )
   }
-  
+
 }
